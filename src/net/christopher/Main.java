3c5bfaf57e508f4449e890cbdc38e913f69c50ab
Christopherjue <cj@phy6.net>
2023-09-09 12:54:17 -0400

    first commit

---
+++ src/net/christopher/Main.java
@@ -0,0 +1,321 @@
+package net.christopher;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        String[] questions = new String[3];
+        questions[0] = "How many states does the USA have?";
+        questions[1] = "what is the capital of the United Kingdom";
+        questions[2] = "What is the chemical symbol for Iron?";
+
+
+        String[] answer = new String[3];
+        answer[0] = "50";
+        answer[1] = "london";
+        answer[2] = "Fe";
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+
+
+        for (int i = 0; i < questions.length; i++) {
+            System.out.println(questions[i]);
+            System.out.println(answer[i]);
+        }
+
+        for (String question : questions) {
+            System.out.println(question);
+        }
+
+
+        Scanner scanner = new Scanner((System.in));
+        while (true) {
+            System.out.println("do you want to continue");
+            if (scanner.next().equals("yes")) {
+                continue;
+            }else {
+                break;
+            }
+
+        }
+    }
+}
+
+
+//        String question1 = "How many states does the USA have?";
+//        String question2 = "what is the capital of the United Kingdom";
+//        String question3 = "What is the chemical symbol for Iron?";
+//
+//        String answer1 = "50";
+//        String answer2 = "london";
+//        String answer3 = "Fe";
+//
+//        String[] questions = new String[3];
+//        questions[0] = "How many states does the USA have?";
+//        questions[1] = "what is the capital of the United Kingdom";
+//        ;
+//        questions[2] = "What is the chemical symbol for Iron?";
+//
+//
+//        String[] answers = new String[3];
+//        answers[0] = "50";
+//        answers[1] = "london";
+//        ;
+//        answers[2] = "Fe";
+//
+//
+//        System.out.println(questions[1]);
+//        System.out.println(answers[1]);
+//
+//
+//        questions[0] = "How old is ewan McGregor";
+//        System.out.println(questions[0]);
+//        System.out.println(answers[0]);
+//
+//
+//
+//
+
+
+//        int exam = 65;
+//        int gift = 0;
+//
+//
+//        if (exam >= 50) {
+//            gift = 10;
+//        } else {
+//            gift = 0;
+//        }
+//        System.out.println(gift);
+//
+//        gift = exam >= 50 ? 10 : 0;
+//        System.out.println(gift);
+
+
+//
+//    int loan = 750;
+//    float interestRate = 0.0525f;
+//
+//    float interest = loan * interestRate;
+//    System.out.println(interest);
+//
+//
+//
+//
+
+
+///        String sentence = "It's a wonderful Java Introduction!";
+//        System.out.println(sentence);
+//        System.out.println("Length is: " + sentence.length());
+//
+//        System.out.println("SHOUTING: " + sentence.toUpperCase());
+//        System.out.println("whispering: " + sentence.toLowerCase());
+//        System.out.println(sentence);
+//
+//        System.out.println("The word 'Java' is found at position: " + sentence.indexOf("Java"));
+//
+//    //replace
+//        System.out.println(sentence.replace("Java","C#"));
+//
+//
+//        System.out.println(sentence.startsWith("I"));
+//        System.out.println(sentence.endsWith("n"));
+//
+//
+//    String s = "";
+//    boolean empty = s.isEmpty();
+//    System.out.println((empty));
+//
+//    System.out.println((sentence.contains("Java")));
+//
+//
+//    System.out.println(sentence.charAt(17));
+//
+//    System.out.println(sentence.substring(17));
+//
+//
+//
+//
+
+
+///       int score = 0;
+///
+///      Scanner Scanner = new Scanner(System.in);
+//        System.out.println("what is the biggest minecraft mod ever created : ");
+//        String v1 = Scanner.nextLine();
+//        String a1 = "aether mod";
+//
+//
+//        if (a1.equals(v1)) {
+//            System.out.println("correct current score 1 out of 3");
+//            score++;
+//        } else {
+//            System.out.println("wrong .current score 0 out of 3");
+//
+//        }
+//        //----------------------------------------------
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("what is the most popular and used in modpacks mod : ");
+//        String v2 = Scanner.nextLine();
+//        String a2 = "jei";
+//
+//
+//        if (a2.equals(v2)) {
+//            System.out.println("correct current score 2 out of 3");
+//            score++;
+//        } else {
+//            System.out.println("wrong .current score 0 out of 3");
+//        }
+//        //----------------------------------------------
+//        Scanner S2 = new Scanner(System.in);
+//        System.out.println("what was the first shadder ever created  : ");
+//        String v3 = Scanner.nextLine();
+//        String a3 = "GLSL";
+//
+//
+//        if (a3.equals(v3)) {
+//            System.out.println("correct current score 3 out of 3 you win");
+//            score++;
+//        } else {
+//            System.out.println("wrong .current score 1 out of 3");
+//        }
+//        //----------------------------------------------
+//  System.out.println(score);
+///        if(Exam2>= 50) {
+//            System.out.println("you have passed exam 2");
+//        } else {
+//            System.out.println("you have FAILED exam 2");
+//        }
+
+
+//        int medalPlacement = 3;
+//
+//        switch(medalPlacement) {
+//           case 1: System.out.println("GOLD MEDAL"); break;
+//           case 2: System.out.println("SILVER MEDAL"); break;
+//           case 3: System.out.println("BRONZE MEDAL"); break;
+//            default: System.out.println("NO MEDAL"); break;
+//        }
+//
+//
+
+//    int Exam1 = 65;
+//    int Exam2 = 51;
+//
+//    Scanner = new Scanner(System.in);
+//    System.out.println("results for exam 1: ");
+//    Exam1 = scanner.nextInt();
+//    System.out.println("results for exam 2: ");
+//    Exam2 = scanner.nextInt();
+//
+//    if(Exam1>= 50) {
+//        System.out.println("you have passed exam 1");
+//    } else {
+//        System.out.println("you have FAILED exam 1");
+//    }
+//
+//        if(Exam2>= 50) {
+//            System.out.println("you have passed exam 2");
+//        } else {
+//            System.out.println("you have FAILED exam 2");
+//        }
+//
+//        if(Exam1 >= 50 && Exam2 >= 50) {
+//            System.out.println("you have Passed The CLASS");
+////        }
+//    }
+//}
+////
+
+
+//        boolean watchedLectures = true;
+//        boolean askedQuestion = false;
+//        boolean finishedAssignments = true;
+//        boolean gaveReview = false;
+//
+//        boolean finishedCourse = watchedLectures && finishedAssignments;
+//        boolean IsAFan = finishedCourse && gaveReview;
+//        boolean learning = watchedLectures || askedQuestion;
+//    boolean likesTheLectures = true;
+//    boolean askedQuestion = false;
+//
+//
+//
+//    System.out.println("you like these lectures " + likesTheLectures);
+//        System.out.println("you have asked a question in the Q&A " + askedQuestion);
+//
+//
+//
+//
+//
+//    int YourScore = 100 ;
+//
+//
+//    boolean passedClass = YourScore >= 50;
+//
+//    System.out.println("you passed: " + passedClass + " with  " + YourScore + "  points! ");
+//
+//
+//    boolean hasPerfectScore = YourScore == 100;
+//    boolean failedClass = !passedClass;
+//
+//
+//    int x = 100;
+//    int y = 20;
+//    x = x + y ;
+//    x += y;
+//    System.out.println(x);
+//    x -= y;
+//    System.out.println(x);
+//    x *= y;
+//    System.out.println(x);
+//    x /= y;
+//    System.out.println(x);
+//    x %= y;
+//    System.out.println(x);
+//    x = x + 1;
+//    x += 1;
+//    x++;
+//    System.out.println(x);
+//
+//    x--;
+//    System.out.println(x);
+//
+//
+//    int result;
+//    result = x + y;
+//    System.out.println((result));
+//    result = x - y;
+//    System.out.println((result));
+//        result = x * y;
+//        System.out.println((result));
+//        result = x / y;
+//        System.out.println((result));
+//        result = 20 % 2;
+//        System.out.println((result));
+//    System.out.println(Math.max(x, y));
+//    System.out.println(Math.abs(-500));
+//System.out.println("Hello world");
+//        int x = 10;
+//    System.out.println(x);
+//
+//
+//    System.out.println("outputting some value "+ x);
+//
+//
+//        Scanner = new Scanner(System.in);
+//        System.out.println("Type in your Username");
+//        String input = scanner.next();
+//
+//
+//
+//        System.out.println("Your Username is: " + input);
+//```
+//
+//
+//
